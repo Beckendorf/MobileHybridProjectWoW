0dc9f97fc47665ffecaa85653df95e37f37bd7ef
MagnumBeck <zoltan.chassot@gmail.com>
2017-11-04 16:07:25 +0100

    Second push

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MobileHybridProjectWoW
+
+###Principe
+
+Application lié au jeu vidéo World of Warcraft permettant de charger un personnage World of Warcraft
+
+
+
+
+
+### Membres du groupe :
+
+- Arthur Chassot
+- Léa Giffard Hontabat
+
+**Nous avons assisté au cours.**
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,23 +2,46 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 // We import the authentication provider to test the log-out function.
 import { AuthProvider } from '../../providers/auth/auth';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+	public items: Array<any> = [];
+	public itemRef: firebase.database.Reference = firebase.database().ref('/items/');
+	
+	constructor(public navCtrl: NavController, public authProvider: AuthProvider) { 
+	}
+		
 
-  constructor(public navCtrl: NavController, public authProvider: AuthProvider) {}
+	ionViewDidLoad() {
+		this.itemRef.on('value', itemSnapshot => {
+			this.items = [];
+			itemSnapshot.forEach( itemSnap => {
+				this.items.push(itemSnap.val());
+				return false;
+			});
+		});
+	}
 
-  /**
-   * Calls the authentication provider and logs the user out, on successful logout it sends the user
-   * back to the login page.
-   */
-  logMeOut() {
-    this.authProvider.logoutUser().then( () => {
-      this.navCtrl.setRoot('LoginPage');
-    });
-  }
+	addItem(name: string): void {
+		var arrayItem={};
+		arrayItem["value"] = name;
+		this.itemRef.update("items", arrayItem);
+	}
+
+	
+	removeItem(item: string): void {
+		this.itemRef.value.remove(item);
+	}
+  
+	logMeOut() {
+		this.authProvider.logoutUser().then( () => {
+		this.navCtrl.setRoot('LoginPage');
+		});
+	}
 
 }
